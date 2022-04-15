da3a07016d9afc01c9edbe77b19d511187258229
“Zhuofan <1718320585@qq.com>
2022-04-16 00:08:18 +0800

    make a list by range

--- 1forlist/forlist.py
+++ 1forlist/forlist.py
@@ -1,8 +1,6 @@
 list1=[1,3,5,7,9]
-i=len(list1)-1
-for cout in range(i):
-    print (list1 [cout])
-i=0
-while i<len(list1):
-    print (list1[i])
-    i+=1
+i=1
+for i ,getv in enumerate(list1):
+    print (i,"  ",getv) 
+ele=list(range(1,6))
+print (ele) 
