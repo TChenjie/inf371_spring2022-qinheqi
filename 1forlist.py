7865fe4bac4aafb93407b9174bd56b713b72abde
“Zhuofan <1718320585@qq.com>
2022-04-16 11:21:37 +0800

    sort the char list

--- 1forlist.py
+++ 1forlist.py
@@ -1,5 +1,8 @@
 list1=[1,2,3,6,9,4,7,8,5]
 print (list1)
 list1=['ab','cd','ef','gh','ac','be']
-print (list1[2]) 
+print (list1[2].title()) 
+
 print (list1[2].title())
+list1.sort()
+print (list1)
