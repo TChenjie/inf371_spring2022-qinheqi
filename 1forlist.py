3363cae35e60babfd8d7e3fe5db2e786f28b02bf
“Zhuofan <1718320585@qq.com>
2022-04-16 11:32:57 +0800

    print list

--- 1forlist.py
+++ 1forlist.py
@@ -1,13 +1,2 @@
-list1=[1,3,5,7,9]
-i=len(list1)-1
-for cout in range(i):
-    print (list1 [cout])
-i=0
-while i<len(list1):
-    print (list1[i])
-    i+=1 
-i=1
-for i ,getv in enumerate(list1):
-    print (i,"  ",getv) 
-ele=list(range(1,6))
-print (ele)  
+list1=[1,5,9,5,6,2,4,4]
+print (list1)
