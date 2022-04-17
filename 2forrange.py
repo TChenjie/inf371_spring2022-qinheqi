fc36b7e1f2bcc465967cbf7f9b98fe0809037433
“Zhuofan <1718320585@qq.com>
2022-04-17 15:53:30 +0800

    use random.getranbits to achieve

--- 2forrange.py
+++ 2forrange.py
@@ -1,6 +1,6 @@
 import random
 num=[]
 for i in range(0,10):
-    num.append(random.choice(['a','b','c','d','e','f']))
+    num.append(random.getrandbits(4))
 for i in range(0,10):
     print (num[i])
