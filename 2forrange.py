b1e9c7f4c02ca080e849980b60c1815f7892f2d3
“Zhuofan <1718320585@qq.com>
2022-04-17 15:45:03 +0800

    use random.choice to achieve

--- 2forrange.py
+++ 2forrange.py
@@ -1,6 +1,6 @@
 import random
 num=[]
 for i in range(0,10):
-    num.append(random.uniform(0,10))
+    num.append(random.choice(['a','b','c','d','e','f']))
 for i in range(0,10):
     print (num[i])
