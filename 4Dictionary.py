3b088f44f90eef86ae88bca8fd1c662408ac7a8b
“Zhuofan <1718320585@qq.com>
2022-04-24 13:12:23 +0800

    test with speed

--- 4Dictionary.py
+++ 4Dictionary.py
@@ -8,6 +8,7 @@ thisdic={
 }
 print(thisdic)
 b=0
+j=0
 for a in thisdic.keys():
     if a=="strength":
         b+=1
@@ -15,3 +16,10 @@ if b!=0:
     print (thisdic["strength"])
 else:
     print("strength isn't a character stat")
+for i in thisdic.keys():
+    if i=="speed":
+        j+=1
+if j!=0:
+    print (thisdic["speed"])
+else:
+    print("speed isn't a character stat")
