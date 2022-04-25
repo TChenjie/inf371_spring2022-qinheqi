f12263790d3f8a60b1af5fcec32c60b364f86307
“Zhuofan <1718320585@qq.com>
2022-04-25 20:08:20 +0800

    Create a properties for class

--- 7classorobjects.py
+++ 7classorobjects.py
@@ -1,2 +1,10 @@
 import random
 class Character:
+     def __init__(self,strength, dexterity, constitution, intelligence, wisdom, charisma):
+        self.strength=strength
+        self.dexterity=dexterity
+        self.constitution=constitution
+        self.intelligence=intelligence
+        self.wisdom=wisdom
+        self.charisma=charisma
+        self.hitpoints=constitution*30+50
