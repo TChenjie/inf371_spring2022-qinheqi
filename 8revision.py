2173a33bce186804b6d4ce360a7ab053bc6e8155
“Zhuofan <1718320585@qq.com>
2022-04-26 11:43:46 +0800

    corrected a mistake

--- 8revision.py
+++ 8revision.py
@@ -15,24 +15,29 @@ class Character:
         print_hitpoints=f"hitpoint is {self.hitpoints}"
         return print_hitpoints
      def attack(self):
-        return random.randrange(1,self.strength)
+        attack1=random.randrange(1,self.strength)
+        return attack1
      def defense(self,defense1):
           self.defense1=defense1
           a=random.randrange(1,20)
           if(a<self.dexterity):
              return 0
           else:
-             self.attack-=defense1
-             return self.attack
+             b=0
+             b=self.attack()
+             b=b-defense1
+             return b
      def heal(self,heal1):
          self.heal1=heal1
          self.hitpoints+=heal1
          return self.hitpoints
 def main():
-    my__new__character=Character(15,15,15,15,15,15)
+    my__new__character=Character(15,15,1,15,15,15)
     print(my__new__character.printststs())
     print(my__new__character.hitpoints1())
     att= my__new__character.attack()
+    print(my__new__character.defense(att))
+    print(my__new__character.defense(att))
     print(my__new__character.defense(att))
     print(my__new__character.heal(20))
 
