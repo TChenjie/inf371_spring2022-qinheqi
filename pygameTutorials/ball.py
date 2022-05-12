21fd55808ce038a2c276c2c617822313b1070e62
“Zhuofan <1718320585@qq.com>
2022-05-12 14:24:12 +0800

    def move method

--- pygameTutorials/ball.py
+++ pygameTutorials/ball.py
@@ -22,6 +22,7 @@ testRect=pygame.Rect(0,0,50,50)
 test=pygame.draw.rect(testplace,blue,testRect)
 ball=("intro_ball.gif",size1,ballspeed)
 smiley=("smiley.png",size2,ballspeed)
-
+def move(self):
+    
 
 
