c607ea0a157d3ebc39d379ea9577939e465a264b
“Zhuofan <1718320585@qq.com>
2022-05-15 21:39:10 +0800

    define the back

--- pygameTutorials/ball.py
+++ pygameTutorials/ball.py
@@ -1,14 +1,15 @@
 import pygame,random
 pygame.init()
 
-size1=width,height=640,480
-size2=width,height=800,600
 
 black=(0,0,0)
 white=(255,255,255)
 blue=(0,0,120)
 backgroundClolor=(50,50,50)
-
+testplace=pygame.Surface((50,50))
+testRect=pygame.Rect(0,0,50,50)
+test=pygame.draw.rect(testplace,blue,testRect)
+yyue=pygame.mixer.music.load("m87.mp3")
 
 
 
