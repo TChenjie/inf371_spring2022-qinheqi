28c4492872e263590380532c858a55220e532d90
“Zhuofan <1718320585@qq.com>
2022-05-15 13:38:21 +0800

    change file loacl

--- pygameTutorials/ball.py
+++ pygameTutorials/ball.py
@@ -32,5 +32,6 @@ test=pygame.draw.rect(testplace,blue,testRect)
 ball=("intro_ball.gif",size1,ballspeed)
 smiley=("smiley.png",size2,ballspeed)
 yyue=pygame.mixer.music.load("m87.mp3")
+key=True
 
 
