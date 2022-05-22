dea976f3063d615149b75e4d926468547740ac90
“Zhuofan <1718320585@qq.com>
2022-05-22 20:51:22 +0800

    Define  medium difficulty implementation

--- project1/AircraftBattle.py
+++ project1/AircraftBattle.py
@@ -4,5 +4,8 @@ pygame.init()
 print("easy\n")
 print("medium\n")
 print("hard\n")
-Hard=input("请选择难度")if(Hard=="easy"):
+Hard=input("请选择难度")
+if(Hard=="easy"):
     dif=5
+elif(Hard=="medium"):
+    dif=15
