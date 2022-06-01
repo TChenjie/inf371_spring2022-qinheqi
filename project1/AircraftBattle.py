1c874613f56a2c364128233869059ed719b01a33
“Zhuofan <1718320585@qq.com>
2022-06-01 15:24:33 +0800

    Initial height

--- project1/AircraftBattle.py
+++ project1/AircraftBattle.py
@@ -38,3 +38,4 @@ class Background():
     def __init__(self, is_alt = 0):
         self.back = pygame.image.load("3.png")
         self.back_rect = self.back.get_rect()
+        self.back_rect.y = is_alt
