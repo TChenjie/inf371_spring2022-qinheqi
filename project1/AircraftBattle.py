9fae38c4f996f4e9c62f9b8491edef16917da96b
“Zhuofan <1718320585@qq.com>
2022-06-14 19:08:32 +0800

    define rect of ENEMY1

--- project1/AircraftBattle.py
+++ project1/AircraftBattle.py
@@ -24,6 +24,7 @@ score = 0
 fighter = 3
 #define rect of myplane
 Myplane_rect=pygame.Rect(100,500,60,60)
+#define rect of ENEMY1
 MY_ENDEVENT1 = pygame.USEREVENT + 1  
 MY_ENDEVENT2 = pygame.USEREVENT + 1  
 MY_ENDEVENT3 = pygame.USEREVENT + 1  
