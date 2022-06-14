466c92305d43641b9ac9f66770dfb40592663a52
“Zhuofan <1718320585@qq.com>
2022-06-14 19:20:43 +0800

    Define the screen

--- project1/AircraftBattle.py
+++ project1/AircraftBattle.py
@@ -55,6 +55,7 @@ Enemy_x=random.randint(0,300)
 jiange1=random.randint(60,200)
 #Defines the refresh interval between remaining enemy aircraft and previous enemy aircraft
 jiange2=random.randint(60,200)
+#Define the screen
 screen = pygame.display.set_mode((480, 700))
 Enemy1_rect=pygame.Rect(Enemy_x,0,60,60)
 Enemy2_rect=pygame.Rect(Enemy_x,0,60,60)
