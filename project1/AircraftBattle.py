a205286c3ebaf706877b19c4ca6610ff8c0b9870
“Zhuofan <1718320585@qq.com>
2022-06-14 19:03:28 +0800

    add comments

--- project1/AircraftBattle.py
+++ project1/AircraftBattle.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 pygame.init()
+#Output difficulty selection
 print("easy\n")
 print("medium\n")
 print("hard\n")
@@ -12,7 +13,7 @@ elif(Hard=="medium"):
 elif(Hard=="hard"):
     dif=8
 else:
-    print("input error ！")
+    print("input error !")
     pygame.quit()
 font1 = pygame.font.SysFont("kaiti", 30)
 font2 = pygame.font.SysFont("kaiti", 30)
@@ -22,27 +23,16 @@ Myplane_rect=pygame.Rect(100,500,60,60)
 MY_ENDEVENT1 = pygame.USEREVENT + 1  
 MY_ENDEVENT2 = pygame.USEREVENT + 1  
 MY_ENDEVENT3 = pygame.USEREVENT + 1  
-if Hard=="easy":
-    bgm1= pygame.mixer.music.load("zhou.mp3")
-    pygame.mixer.music.play()
-    pygame.mixer.music.set_endevent(MY_ENDEVENT1)
-if Hard=="medium":
-    bgm2= pygame.mixer.music.load("huoyuanjia.mp3")
-    pygame.mixer.music.play()#播放音乐
-    pygame.mixer.music.set_endevent(MY_ENDEVENT2)
-if Hard=="hard":
-    bgm3= pygame.mixer.music.load("CompendiumOfMateriaMedica.mp3")
-    pygame.mixer.music.play()#播放音乐
-    pygame.mixer.music.set_endevent(MY_ENDEVENT3)
+
 class Background():
-    def __init__(self, is_alt = -1000):
+    def __init__(self, is_alt = -700):
         self.back = pygame.image.load("3.png")
         self.back_rect = self.back.get_rect()
         self.back_rect.y = is_alt
     def update(self):
         self.back_rect.y += 1
-        if self.back_rect.centery >= 1000:
-            self.back_rect.y = -1000
+        if self.back_rect.y >= 700:
+            self.back_rect.y = -700
     def draw(self):
         screen.blit(self.back,self.back_rect)
 Enemy_x=random.randint(0,300)
@@ -53,5 +43,5 @@ Enemy1_rect=pygame.Rect(Enemy_x,0,60,60)
 Enemy2_rect=pygame.Rect(Enemy_x,0,60,60)
 Enemy3_rect=pygame.Rect(Enemy_x,0,60,60)
 bulte_rect=pygame.Rect(100,450,30,69)
-bg1=Background()
-bg2=Background(-1000)
+bg1=Background(0)
+bg2=Background(-700)
